02a349eed7d9f82d38f513783ce856088edfa282
JYPPAP <yungsu22276@gmail.com>
2025-04-04 02:27:32 +0900

    Updates

--- src/constants/basePath.js
+++
@@ -1,3 +0,0 @@
-const isProduct = process?.env?.NODE_DEV === "production";
-
-export const BASE_PATH = isProduct ? "/front_5th_chapter1-2" : "";
--- src/lib/createVNode.js
+++
@@ -1,9 +0,0 @@
-export function createVNode(type, props, ...children) {
-  return {
-    type,
-    props,
-    children: children
-      .flat(Infinity)
-      .filter((node) => node === 0 || Boolean(node)),
-  };
-}
